c248dde2bbc69d8262d5503bfd836240b81c0614
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-03-06 21:40:43 +0100

    frame state machine draft

--- frame.c
+++ frame.c
@@ -44,3 +44,94 @@
  * 
  */
 // ------------------------
+
+typedef enum {
+    FORWARD,
+    BACKWARD,
+    TURN_RIGHT,
+    TURN_LEFT
+} command;
+
+typedef struct {
+    command cmd;
+    unsigned int param;
+} frame_data;
+
+typedef enum {
+    START_BIT,
+    DATA, // needed?
+    PARITY,
+    STOP_BIT
+} frame_state;
+
+typedef struct {
+    frame_state start_bit;
+    frame_data data;
+    frame_state parity;
+    frame_state stop_bit;
+} frame;
+
+// TODO change function name
+int TODO_CHANGE_FCTN_NAME() {
+
+    frame_state frame_state = START_BIT;
+    frame frame;
+    frame.data.cmd = FORWARD;
+    frame.data.param = 10;
+    int parity = 0; // even parity, or odd parity?
+    int started = 0;
+    int stopped = 0;
+
+    //TODO state machine ?
+
+    switch (frame_state) {
+        case START_BIT:
+            // TODO
+            started = 1;
+            stopped = 0;
+            break;
+        case DATA:
+            // TODO
+            switch (frame.data.cmd) {
+                case FORWARD:
+                    // TODO
+                    parity++; //TODO
+                    forward(frame.data.param);
+                    break;
+                case BACKWARD:
+                    // TODO
+                    backward(frame.data.param);
+                    break;
+                case TURN_RIGHT:
+                    // TODO
+                    turn_right(frame.data.param);
+                    break;
+                case TURN_LEFT:
+                    // TODO
+                    turn_left(frame.data.param);
+                    break;
+            }
+            break;
+        case PARITY:
+            // TODO
+            if (frame_state == 0 && parity%2 == 0) {// TODO
+                // TODO
+            } else if (frame_state == 1 && parity%2 == 1) {// TODO
+                // TODO
+            } else {
+                // error
+            }
+            break;
+        case STOP_BIT:
+            // TODO
+            stopped = 1;
+            started = 0;
+            break;
+        default:
+            // TODO
+            // error?
+            break;
+    }
+
+    return 0;
+}
