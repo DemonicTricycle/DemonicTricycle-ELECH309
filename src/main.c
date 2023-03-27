46ed1728b346d1fb63f5bf4c710553e3d07ef1de
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-03-27 14:19:34 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,14 +4,27 @@
  *
  */
 
+#define DEBUG 1
+
 #include "xc.h" // for xc16 compiler
+#ifdef DEBUG
+#include <stdio.h>
+#endif
 
 
 int main(void) {
+    /// Starting point of the microcontroller program
+    #ifdef DEBUG
+    printf("starting");
+    #endif
 
 
     while(1) {
         // TODO
+        // check for received sound
+        // get data from sound
+        // send command to motors
+        // wait for next sound
     }
 
     return 0;
