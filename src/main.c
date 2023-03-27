9246c239118c797419bc06be10dff431e9caa2cf
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-03-27 12:10:01 +0200

    add configuration.c
    for xc16 compiler, for the microcontroller

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Main file of Demonic Tricycle
+ * Author: Martin Fonteyn, Lucas Placentino, Sylvain Powis de Tenbossche
+ *
+ */
+
+#include "xc.h" // for xc16 compiler
+
+
+int main(void) {
+
+
+    while(1) {
+        // TODO
+    }
+
+    return 0;
+}
