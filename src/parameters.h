f3e96d77975ef27382d7efbbd7a3cdfc709b0939
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-04-14 16:32:46 +0200

    add a parameters.h file
    containing all global parameters

---
+++ src/parameters.h
@@ -0,0 +1,15 @@
+// parameters defines
+
+#ifndef FCY
+#define FCY 3685000
+#endif
+
+#ifndef WHEELBASE
+#define WHEELBASE 0.135
+#endif
+
+#ifndef WHEEL_RADIUS
+#define WHEEL_RADIUS 0.04 // in m
+#endif
+
+
